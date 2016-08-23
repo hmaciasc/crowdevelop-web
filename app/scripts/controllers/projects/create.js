7fb696c51b09d97d3291383904262576e8cc9035
Héctor Macías <hectormaciasc@gmail.com>
2016-08-23 23:46:46 +0100

    Project creation
    Project creation form implemented using responsive

--- app/scripts/controllers/projects/create.js
+++ app/scripts/controllers/projects/create.js
@@ -5,6 +5,7 @@ angular.module('crowDevelop')
 .controller('ProjectsCreateCtrl', ['$scope', '$firebaseObject', '$location', function($scope, $firebaseObject, $location) {
 
     $scope.categories = ["Development", "Game", "Education", "Social"];
+
     $scope.name;
     $scope.category;
     $scope.email;
@@ -14,15 +15,33 @@ angular.module('crowDevelop')
     $scope.selectedDate;
 
     $scope.create = function() {
+        var project = $scope.project;
+
+        $scope.name = $scope.project.name;
+        $scope.category = $scope.project.category;
+        $scope.email = $scope.project.email;
+        $scope.description = $scope.project.description;
+        $scope.goal = $scope.project.goal;
+        $scope.achievement = $scope.project.achievement;
+        $scope.selectedDate = $scope.project.selectedDate;
+
+        console.log($scope.project);
         var ref = firebase.database().ref();
 
-        var obj = $firebaseObject(ref);
+        var obj = $firebaseObject();
+        obj.project = project;
+        obj.$save().then(function(ref) {
+            ref.key === obj.$id; // true
+        }, function(error) {
+            console.log("Error:", error);
+        });
+
 
         obj.$loaded().then(function() {
             $location.path('/');
         });
 
-
+        $location.path('/');
     };
 
 }]);
