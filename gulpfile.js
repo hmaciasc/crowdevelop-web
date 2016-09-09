b04a3022213af0e0c92cba1463e1fcf676b4c727
Héctor Macías Casado <hectormaciasc@gmail.com>
2016-09-09 12:22:25 +0100

    Share buttons redesign. Gulpfile implemented. Npm gulp packages downloaded

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,40 @@
 // Load Gulp
 var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    plugins = require('gulp-load-plugins')({
-        rename: {
-            'gulp-live-server': 'serve'
-        }
-    });
+cssmin = require('gulp-cssmin'),
+less = require('gulp-less'),
+path = require('path'),
+plugins = require('gulp-load-plugins')({
+  rename: {
+    'gulp-live-server': 'serve',
+    'gulp-autoprefixer': 'autoprefixer'
+  }
+});
 
 // Start Watching: Run "gulp"
 gulp.task('default', ['watch']);
 
-// Less to CSS: Run manually with: "gulp build-css"
-gulp.task('build-css', function () {
-    return gulp.src('app/styles/*.less')
-        .pipe(plugins.plumber())
-        .pipe(plugins.less())
-        .on('error', function (err) {
-            gutil.log(err);
-            this.emit('end');
-        })
-        .pipe(plugins.autoprefixer({
-            browsers: [
-                    '> 1%',
-                    'last 2 versions',
-                    'firefox >= 4',
-                    'safari 7',
-                    'safari 8',
-                    'IE 8',
-                    'IE 9',
-                    'IE 10',
-                    'IE 11'
-                ],
-            cascade: false
-        }))
-        .pipe(plugins.cssmin())
-        .pipe(gulp.dest('app/styles')).on('error', gutil.log);
+gulp.task('less', function(){
+  gulp.src('app/styles/less/*.less')
+  .pipe(less())
+  .pipe(plugins.autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+  }))
+  .pipe(gulp.cssmin())
+  .pipe(gulp.dest('app/styles/css'));
 });
 
 // Default task
 gulp.task('watch', function () {
-    gulp.watch('app/styles/**/*.less', ['build-css']);
+  gulp.watch('app/styles/less/**/*.less', ['less']);
 });
 
 // Folder "/" serving at http://localhost:8888
 // Should use Livereload (http://livereload.com/extensions/)
 gulp.task('serve', function () {
-    var server = plugins.serve.static('/', 8888);
-    server.start();
-    gulp.watch(['build/*'], function (file) {
-        server.notify.apply(server, [file]);
-    });
+  var server = plugins.serve.static('app/', 5000);
+  server.start();
+  gulp.watch(['app/*'], function (file) {
+    server.notify.apply(server, [file]);
+  });
 });
