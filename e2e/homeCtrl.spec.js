016c0d7688bbca7f02ff88b4d73cd3cb3e8795ff
Héctor Macías <hectormaciasc@gmail.com>
2016-09-24 17:15:54 +0100

    home view tests
    Home view test implemented

---
+++ e2e/homeCtrl.spec.js
@@ -0,0 +1,32 @@
+// describe('HopmeCtrl', function() {
+//     beforeEach(module('crowDevelop'));
+//
+//     var $controller;
+//
+//     beforeEach(inject(function(_$controller_) {
+//         // The injector unwraps the underscores (_) from around the parameter names when matching
+//         $controller = _$controller_;
+//     }));
+//
+//     describe('$scope.findProject', function() {
+//         it('gets a list of projects from search form', function() {
+//             var $scope = {};
+//             var controller = $controller('PasswordController', {
+//                 $scope: $scope
+//             });
+//             $scope.query = 'asd';
+//             $scope.findProject();
+//             expect($scope.projectSearch).not.toBe(null);;
+//         });
+//     });
+//     describe('$scope.init', function() {
+//         it('gets a list of projects from db', function() {
+//             var $scope = {};
+//             var controller = $controller('PasswordController', {
+//                 $scope: $scope
+//             });
+//             $scope.init();
+//             expect($scope.projects.count()).not.toBe(0);
+//         });
+//     });
+// });
