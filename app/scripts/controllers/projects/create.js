0f1181cfd983067f03d153185b2f7fa82238ab91
Héctor Macías <hectormaciasc@gmail.com>
2016-10-22 22:39:21 +0100

    Image upload
    Project creation image upload

--- app/scripts/controllers/projects/create.js
+++ app/scripts/controllers/projects/create.js
@@ -2,7 +2,7 @@
 
 angular.module('crowDevelop')
 
-.controller('ProjectsCreateCtrl', ['$rootScope', '$scope', '$firebaseObject', '$location', function($rootScope, $scope, $firebaseObject, $location) {
+.controller('ProjectsCreateCtrl', ['$rootScope', '$scope', '$firebaseArray', '$location', function($rootScope, $scope, $firebaseArray, $location) {
     $scope.project = {};
     $scope.currentDate = new Date();
 
@@ -18,12 +18,9 @@ angular.module('crowDevelop')
         $scope.myDate.getMonth() + 1,
         $scope.myDate.getDate());
 
-    // window.sco = $scope;
-
     $scope.log = function(image) {
         $scope.project.image = image.files[0];
         window.image = image.files[0];
-        console.log($scope);
         $scope.$apply();
     };
 
@@ -32,22 +29,23 @@ angular.module('crowDevelop')
 
         $scope.project.ownerId = $rootScope.firebaseUser.uid;
         $scope.project.ownerName = $rootScope.firebaseUser.displayName;
-        $scope.project.donated = 0;
         $scope.project.day = $scope.project.selectedDate.getDay();
         $scope.project.month = $scope.project.selectedDate.getMonth() + 1;
         $scope.project.year = $scope.project.selectedDate.getFullYear();
+        $scope.project.donated = 0;
         $scope.project.status = "inProgress";
+        $scope.project.imagesRef = "";
 
-
-        var vid = $scope.project.video.replace("/watch?v=", "/embed/");
+        var vid = $scope.project.videoUrl.replace("/watch?v=", "/embed/");
         var initIndex = vid.lastIndexOf("/");
         var videoId = vid.slice(initIndex + 1, vid.length);
         $scope.project.video = videoId;
 
-        var projectsRef = firebase.database().ref('projects');
-        var project = projectsRef.push();
-        project.set($scope.project);
-        $scope.project.imageRef = uploadPhoto(project.key);
+        var ref = firebase.database().ref('projects');
+        var list = $firebaseArray(ref);
+        list.$add($scope.project).then(function(ref) {
+            uploadPhoto(ref.key);
+        });
 
         $location.path('/');
     };
@@ -56,10 +54,42 @@ angular.module('crowDevelop')
         var storage = firebase.storage();
         var storageRef = storage.ref();
         var imagesRef = storageRef.child('projectImages/' + projectKey).put($scope.project.image);
+        var downUrl = imagesRef.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+            function(snapshot) {
+                var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + progress + '% done');
+                switch (snapshot.state) {
+                    case firebase.storage.TaskState.PAUSED:
+                        console.log('Upload is paused');
+                        break;
+                    case firebase.storage.TaskState.RUNNING:
+                        console.log('Upload is running');
+                        break;
+                }
+            },
+            function(error) {
+                switch (error.code) {
+                    case 'storage/unauthorized':
+                        console.log("User doesn't have permission to access the object");
+                        break;
 
-        console.log(imagesRef);
+                    case 'storage/canceled':
+                        console.log('User canceled the upload');
+                        break;
 
-        return imagesRef;
+                    case 'storage/unknown':
+                        console.log('Unknown error occurred, inspect error.serverResponse');
+                        break;
+                }
+            },
+            function() {
+                $scope.project.imagesRef = imagesRef.snapshot.downloadURL;
+
+                var ref = firebase.database().ref('projects/' + projectKey + '/imageRef');
+                ref.set(imagesRef.snapshot.downloadURL);
+
+                return imagesRef.snapshot.downloadURL;
+            });
     }
 
 }]);
