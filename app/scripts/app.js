dd74cc6bdcd189b66a7c2169f43962b74f11a068
Héctor Macías <hectormaciasc@gmail.com>
2016-11-08 22:04:47 +0000

    Service worker
    Service worker reimplemented
    Google sign in image removed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -77,15 +77,15 @@ angular.module('crowDevelop', ['firebase', 'ngRoute', 'ngStorage', 'ngAnimate'])
 })
 
 .run(['$rootScope', function($rootScope) {
-    // if ('serviceWorker' in navigator) {
-    //     navigator.serviceWorker.register('../sw.js').then(function(registration) {
-    //         // Registration was successful
-    //         console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    //     }).catch(function(err) {
-    //         // registration failed :(
-    //         console.log('ServiceWorker registration failed: ', err);
-    //     });
-    // }
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('../sw.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }).catch(function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    }
 }])
 
 .run(['$rootScope', '$location', '$localStorage', 'AuthService', function($rootScope, $location, $localStorage, AuthService) {
