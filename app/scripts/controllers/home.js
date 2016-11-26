3533d5a5c5ee577c7eb3b230f28512a7329f3273
Héctor Macías <hectormaciasc@gmail.com>
2016-11-26 00:05:58 +0000

    Project ending visual bug

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -20,7 +20,30 @@ angular.module('crowDevelop')
         var projectRef = firebase.database().ref('projects/');
         var query = projectRef.orderByChild('donated').limitToLast(10);
         $scope.projects = $firebaseArray(query);
+        updateStatuses($scope.projects);
     };
+
+
+    function updateStatuses(projects) {
+        projects.$loaded(
+            function(projects) {
+                for (var i = 0; i < projects.length; i++) {
+                    var expireDate = new Date(projects[i].year, projects[i].month, projects[i].day);
+                    var now = new Date();
+                    if (expireDate < now) endProject(projects[i]);
+                }
+            },
+            function(err) {
+                console.log(err);
+            });
+    }
+
+    function endProject(project) {
+        var projectRef = firebase.database().ref('projects/' + project.$id);
+        var project = $firebaseObject(projectRef);
+        project.status = 'closed';
+        project.$save();
+    }
 
     $scope.init();
 }]);
